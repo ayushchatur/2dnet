7d5138403f44387a0bcf9cc5b36b8afabc64e218
ayush <ayush.6200@gmail.com>
2022-06-11 08:49:09 -0400

    disabling nvptx dispatch

--- sparse_ddnet.py
+++ sparse_ddnet.py
@@ -686,7 +686,7 @@ def cleanup():
     dist.destroy_process_group()
 
 import nvidia_dlprof_pytorch_nvtx
-nvidia_dlprof_pytorch_nvtx.init(enable_function_stack=True)
+# nvidia_dlprof_pytorch_nvtx.init(enable_function_stack=True)
 from apex.contrib.sparsity import ASP
 def dd_train(gpu, args):
     rank = args.nr * args.gpus + gpu
