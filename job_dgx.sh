03b4c09fe322d3917cca7a229fc14eaeb49dc16e
ayush chaturvedi <ayush.6200@gmail.com>
2022-07-02 09:59:28 -0400

    adding jog_dgx

--- job_dgx.sh
+++ job_dgx.sh
@@ -10,4 +10,6 @@ export MASTER_PORT=8882
 export SLURM_PROCID=0
 export append=$RANDOM
 touch output_$append.out
+echo "output_$append.out"
+docker run --rm --gpus all -it -v ~/Downloads:/projects/synergy_lab/garvit217/enhancement_data/ nvcr.io/nvidia/pytorch:21.12-py3 \ 
 python sparse_ddnet.py -n 1 -g 1 --batch $batch_size --epochs $epochs --retrain $retrain > output_$append.out &
