b6b01a73d89361081b2e0e8f1e79ad0477a08995
Ayush Chaturvedi <ayush.6200@gmail.com>
2023-03-16 17:04:22 +0000

    fixing n tasks

--- job_tinker.sh
+++ job_tinker.sh
@@ -17,7 +17,7 @@ master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_ADDR=$master_addr
 echo "MASTER_ADDR="$MASTER_ADDR
 export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
-export "world size=" $WORLD_SIZE
+echo "WORLD_SIZE="$WORLD_SIZE
 echo "slurm job: $SLURM_JOBID"
 #expor job_id=$SLURM_JOBID
 mkdir -p $SLURM_JOBID;cd $SLURM_JOBID
