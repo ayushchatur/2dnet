fixing profiling and updating mixed precision stuff

sparse_ddnet.py
@@ -732,6 +732,12 @@ def dd_train(args):
     num_w = args.num_w
     en_wan = args.wan
 
+    model = DD_net()
+    device = torch.device("cuda", local_rank)
+    torch.cuda.manual_seed(1111)
+    # necessary for AMP to work
+    torch.cuda.set_device(device)
+    model.to(device)
 
     root_train_h = "/projects/synergy_lab/garvit217/enhancement_data/train/HQ"
     root_train_l = "/projects/synergy_lab/garvit217/enhancement_data/train/LQ"
@@ -747,8 +753,6 @@ def dd_train(args):
 
 
 
-    model = DD_net()
-    model.to(local_rank)
 
     if gr_mode != "none":
         model = DDP(model, device_ids=[local_rank])

sparse_ddnet_old_dl.py
@@ -729,7 +729,11 @@ def dd_train(args):
 
     dist.barrier()
     model = DD_net()
-    model.to(local_rank)
+    device = torch.device("cuda", local_rank)
+    torch.cuda.manual_seed(1111)
+    # necessary for AMP to work
+    torch.cuda.set_device(device)
+    model.to(device)
 
     if gr_mode != "none":
         model = DDP(model, device_ids=[local_rank])

sparse_ddnet_pp.py
@@ -640,6 +640,84 @@ def setup(rank, world_size):
 
 import nvidia_dlprof_pytorch_nvtx
 nvidia_dlprof_pytorch_nvtx.init(enable_function_stack=True)
+def read_correct_image(path):
+    offset = 0
+    ct_org = None
+    with Image.open(path) as img:
+        ct_org = np.float32(np.array(img))
+        if 270 in img.tag.keys():
+            for item in img.tag[270][0].split("\n"):
+                if "c0=" in item:
+                    loi = item.strip()
+                    offset = re.findall(r"[-+]?\d*\.\d+|\d+", loi)
+                    offset = (float(offset[1]))
+    ct_org = ct_org + offset
+    neg_val_index = ct_org < (-1024)
+    ct_org[neg_val_index] = -1024
+    return ct_org
+class CTDataset(Dataset):
+    def __init__(self, root_dir_h, root_dir_l, length, transform=None):
+        self.data_root_l = root_dir_l + "/"
+        self.data_root_h = root_dir_h + "/"
+        self.img_list_l = os.listdir(self.data_root_l)
+        self.img_list_h = os.listdir(self.data_root_h)
+        self.img_list_l.sort()
+        self.img_list_h.sort()
+        self.img_list_l = self.img_list_l[0:length]
+        self.img_list_h = self.img_list_h[0:length]
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.img_list_l)
+
+    def __getitem__(self, idx):
+        # print("Dataloader idx: ", idx)
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        inputs_np = None
+        targets_np = None
+        rmin = 0
+        rmax = 1
+
+        # print("HQ", self.data_root_h + self.img_list_h[idx])
+        # print("LQ", self.data_root_l + self.img_list_l[idx])
+        # image_target = read_correct_image("/groups/synergy_lab/garvit217/enhancement_data/train/LQ//BIMCV_139_image_65.tif")
+        # print("test")
+        # exit()
+        image_target = read_correct_image(self.data_root_h + self.img_list_h[idx])
+        image_input = read_correct_image(self.data_root_l + self.img_list_l[idx])
+
+        input_file = self.img_list_l[idx]
+        assert (image_input.shape[0] == 512 and image_input.shape[1] == 512)
+        assert (image_target.shape[0] == 512 and image_target.shape[1] == 512)
+        cmax1 = np.amax(image_target)
+        cmin1 = np.amin(image_target)
+        image_target = rmin + ((image_target - cmin1) / (cmax1 - cmin1) * (rmax - rmin))
+        assert ((np.amin(image_target) >= 0) and (np.amax(image_target) <= 1))
+        cmax2 = np.amax(image_input)
+        cmin2 = np.amin(image_input)
+        image_input = rmin + ((image_input - cmin2) / (cmax2 - cmin2) * (rmax - rmin))
+        assert ((np.amin(image_input) >= 0) and (np.amax(image_input) <= 1))
+        mins = ((cmin1 + cmin2) / 2)
+        maxs = ((cmax1 + cmax2) / 2)
+        image_target = image_target.reshape((1, 512, 512))
+        image_input = image_input.reshape((1, 512, 512))
+        inputs_np = image_input
+        targets_np = image_target
+
+        inputs = torch.from_numpy(inputs_np)
+        targets = torch.from_numpy(targets_np)
+        inputs = inputs.type(torch.FloatTensor)
+        targets = targets.type(torch.FloatTensor)
+
+        sample = {'vol': input_file,
+                  'HQ': targets,
+                  'LQ': inputs,
+                  'max': maxs,
+                  'min': mins}
+        return sample
+
 def cleanup():
     dist.destroy_process_group()
 torch.backends.cudnn.benchmark=True
@@ -681,6 +759,13 @@ def dd_train(args):
     root_test_h = "/projects/synergy_lab/garvit217/enhancement_data/test/HQ/"
     root_test_l = "/projects/synergy_lab/garvit217/enhancement_data/test/LQ/"
 
+    model = DD_net()
+    device = torch.device("cuda", local_rank)
+    torch.cuda.manual_seed(1111)
+    # necessary for AMP to work
+    torch.cuda.set_device(device)
+    model.to(device)
+
     trainset = CTDataset(root_dir_h=root_train_h, root_dir_l=root_train_l, length=5120)
     testset = CTDataset(root_dir_h=root_test_h, root_dir_l=root_test_l, length=784)
     valset = CTDataset(root_dir_h=root_val_h, root_dir_l=root_val_l, length=784)
@@ -700,11 +785,7 @@ def dd_train(args):
 
     dist.barrier()
 
-    model = DD_net()
 
-    # torch.cuda.set_device(rank)
-    # model.cuda(rank)
-    model.to(local_rank)
     model = DDP(model, device_ids=[local_rank])
     learn_rate = 0.0001;
     epsilon = 1e-8

sparse_ddnet_pro.py
@@ -705,8 +705,16 @@ def setup(rank, world_size):
     # initialize the process group
     dist.init_process_group("gloo", rank=rank, world_size=world_size)
 
-import nvidia_dlprof_pytorch_nvtx
-nvidia_dlprof_pytorch_nvtx.init(enable_function_stack=True)
+# import nvidia_dlprof_pytorch_nvtx
+# nvidia_dlprof_pytorch_nvtx.init(enable_function_stack=True)
+
+from ctypes import cdll
+libcudart = cdll.LoadLibrary('libcudart.so')
+def cudaProfilerStart():
+    libcudart.cudaProfilerStart()
+def cudaProfilerStop():
+    libcudart.cudaProfilerStop()
+
 def cleanup():
     dist.destroy_process_group()
 from socket import gethostname
@@ -751,11 +759,14 @@ def dd_train(args):
     train_loader = CTDataset(root_train_h, root_train_l, 5120, local_rank, batch)
     test_loader = CTDataset(root_test_h, root_test_l, 784, local_rank, batch)
     val_loader = CTDataset(root_val_h, root_val_l, 784, local_rank, batch)
-    model = DD_net()
 
-    # torch.cuda.set_device(rank)
-    # model.cuda(rank)
-    model.to(local_rank)
+    model = DD_net()
+    device = torch.device("cuda", local_rank)
+    torch.cuda.manual_seed(1111)
+    # necessary for AMP to work
+    torch.cuda.set_device(device)
+    model.to(device)
+
     model = DDP(model, device_ids=[local_rank])
     learn_rate = 0.0001;
     epsilon = 1e-8
@@ -778,6 +789,15 @@ def dd_train(args):
     test_MSE_loss = [0]
     test_MSSSIM_loss = [0]
     test_total_loss = [0]
+
+    num_profile_step_size = int(1. / 0.2)
+    profile_rank_list = list(range(0, world_size, num_profile_step_size))
+    if rank in profile_rank_list:
+        start_profiler_handle = cudaProfilerStart
+        stop_profiler_handle = cudaProfilerStop
+    else:
+        start_profiler_handle = None
+        stop_profiler_handle = None
 
     model_file = "weights_" + str(epochs) + "_" + str(batch) + ".pt"
 