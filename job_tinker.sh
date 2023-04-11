1dc1b2caefcc7e533d70b42b927bb7a13992c181
ayushchatur <ayush.6200@gmail.com>
2023-04-11 14:11:09 -0400

    fixing world size

--- job_tinker.sh
+++ job_tinker.sh
@@ -20,7 +20,6 @@ master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
 export MASTER_ADDR=$master_addr
 echo "MASTER_ADDR="$MASTER_ADDR
 export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
-export WORLD_SIZE=1
 echo "slurm job: $SLURM_JOBID"
 #expor job_id=$SLURM_JOBID
 mkdir -p $SLURM_JOBID;cd $SLURM_JOBID
