220d25efcb50b6a880cc8caf32bd007188b9006a
ayush chaturvedi <ayush.6200@gmail.com>
2023-04-24 11:46:00 -0400

    singulait

--- job_tinker.sh
+++ job_tinker.sh
@@ -87,6 +87,7 @@ else
   export imagefile="/projects/synergy_lab/ayush/containers/pytorch_22.04.sif"
 fi
 
+module list
 export BASE="singularity exec --nv --writable-tmpfs --bind=/projects/synergy_lab/garvit217,/cm/shared,${TMPFS} ${imagefile} "
 
 
