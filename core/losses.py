4eb3ac64e2dc5ab70243407e40d285d475c5bc47
ayush chaturvedi <ayush.6200@gmail.com>
2023-04-24 18:39:01 -0400

    ignoring ms-ssim

--- core/losses.py
+++ core/losses.py
@@ -2,12 +2,12 @@ import torch
 from math import exp
 import torch
 import torch.nn.functional as F
-@torch.jit.script
+@torch.jit.ignore
 def gaussian(window_size, sigma):
     gauss = torch.Tensor([exp(-(x - window_size // 2) ** 2 / float(2 * sigma ** 2)) for x in range(window_size)])
     return gauss / gauss.sum()
 
-@torch.jit.script
+@torch.jit.ignore
 def create_window(window_size, channel=1):
     _1D_window = gaussian(window_size, 1.5).unsqueeze(1)
     _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
