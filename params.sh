8f50a2279598401c84abdb64c6928f5d622e00e0
ayush <ayush.6200@gmail.com>
2023-10-24 17:30:55 -0600

    added more comments

--- params.sh
+++ params.sh
@@ -14,22 +14,29 @@ export profile_perct=0.2
 
 
 
-export batch_size=32
+export batch_size=1
 export epochs=50
+# if retrain >0 then only the sparse retraining will enable with below options 
 export retrain=0
+# options mag/l1_struc or random_unstru will be set otherwise
+export prune_t="random_unstru" 
+export prune_amt=0.5
+export model="ddnet"
+
+
 export num_data_w=4
+#enables data stagin 
 export new_load="false"
 export wandb=-1
 export lr=0.001
 export dr=0.95
 export distback="gloo"
-# options mag/l1_struc or random_unstru will be set otherwise
-export prune_t="random_unstru"
-export prune_amt=0.5
-export model="ddnet"
+
+
 #options enable/disable
 export mp="false"
 export schedtype="expo"
+
 #pytorch version 1 or version 2(with new graph support)
 
 export inferonly="false"
